f91010e30b3ef42cbe578b730c4f199bf18da066
Merveil Ewoni <ewoniewonimerveil@gmail.com>
2021-08-16 13:13:12 +0100

    initial commit

---
+++ datipay.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package Dati_Pay
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Datipay
+Plugin URI: 
+Description: plugin de paiement de produit en ligne
+Author: Dati Cloud LLC
+Version: 1.0.0
+*/
+
+include 'functions/main-functions.php';
+
+
+
